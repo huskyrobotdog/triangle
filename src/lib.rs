7e2d4d9f1b3d1b3e8b8b7f9d189a957b8030f075
husky <husky.robot.dog@gmail.com>
2024-04-14 17:30:53 +0800

    coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,55 @@
-pub mod crypto;
-pub mod env;
-pub mod http;
-pub mod log;
-pub mod orm;
+#![allow(unused)]
+
 pub mod runtime;
+
+pub use anyhow;
+pub use tokio;
+
+#[cfg(feature = "serde")]
+pub use serde;
+#[cfg(feature = "serde")]
+pub use serde_urlencoded;
+
+#[cfg(feature = "json")]
+pub use serde_json;
+
+#[cfg(feature = "chrono")]
+pub use chrono;
+
+#[cfg(feature = "env")]
+pub use config;
+#[cfg(feature = "env")]
+pub mod env;
+
+#[cfg(feature = "logger")]
+pub use tracing;
+#[cfg(feature = "logger")]
+pub use tracing_appender;
+#[cfg(feature = "logger")]
+pub use tracing_subscriber;
+#[cfg(feature = "logger")]
+pub mod logger;
+
+#[cfg(feature = "http")]
+pub use reqwest;
+#[cfg(feature = "http")]
+pub mod http;
+
+#[cfg(feature = "uuid")]
+pub use snowflaked;
+#[cfg(feature = "uuid")]
 pub mod uuid;
+
+#[cfg(feature = "crypto")]
+pub use base64;
+#[cfg(feature = "crypto")]
+pub use ring;
+#[cfg(feature = "crypto")]
+pub mod crypto;
+
+#[cfg(feature = "orm")]
+pub use log;
+#[cfg(feature = "orm")]
+pub use sea_orm;
+#[cfg(feature = "orm")]
+pub mod orm;
